f595c4bcf008df5bd8623e73b30130b07d7008d2
NYPMingyue <67146999+NYPMingyue@users.noreply.github.com>
2020-06-25 13:37:18 +0800

    Add files via upload

---
+++ iPrepMgmt/DAO/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iPrepMgmt.DAO
+{
+    public class Account
+    {
+        public int SID { get; set; }
+        public string UserId { get; set; }
+        public int Role { get; set; }
+        public string password { get; set; }
+    }
+}
